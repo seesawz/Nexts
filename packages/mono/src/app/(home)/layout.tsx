c3d21c23d50d162b4017a97e2299ae56643613aa
seesawz <545243476@qq.com>
2024-01-09 23:39:12 +0800

    fix: layout

--- packages/mono/src/app/(home)/layout.tsx
+++ packages/mono/src/app/(home)/layout.tsx
@@ -1,9 +1,12 @@
 import { PropsWithChildren } from "react";
-import { getQueryClient } from "../../lib/query-client.server";
 
 //设置重新验证时间为60s
 export const revalidate = 60
 
 export default async function HomeLayout(props: PropsWithChildren) {
-  const queryClient = getQueryClient()
+  return (
+    <html lang="en">
+      <body>{props.children}</body>
+    </html>
+  )
 }
--- packages/mono/src/app/(home)/page.tsx
+++ packages/mono/src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-
+      <h1>welcome</h1>
     </div>
   )
 }
